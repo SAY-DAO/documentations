954f708056451432332e313dd8458fcb1fb21c68
Gbuntu <ehsan@say.company>
2023-09-13 10:28:15 +0330

    pallet and fonts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,106 +1,105 @@
-import { defineUserConfig } from "@vuepress/cli";
 import { defaultTheme } from "vuepress";
-import { docsearchPlugin } from '@vuepress/plugin-docsearch'
-import { shikiPlugin } from '@vuepress/plugin-shiki'
-import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
-import { path } from '@vuepress/utils'
+import { docsearchPlugin } from "@vuepress/plugin-docsearch";
+import { registerComponentsPlugin } from "@vuepress/plugin-register-components";
+import { path } from "@vuepress/utils";
 
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === "production";
 
-export default defineUserConfig({
-  title: "Vue Docs",
-  description: "Building documentations with Vue using VuePress",
+export default {
   locales: {
     // As a special case, the default locale can use '/' as its path.
     "/": {
       head: [["link", { rel: "icon", href: "/images/favicon.ico" }]],
       lang: "fa-IR",
-      title: "SAY DAO",
-      description: "اسناد توسعه‌‌",
+      title: "اسناد توسعه‌‌",
+      // description: "---",
     },
-    "/en/": {
-      head: [["link", { rel: "icon", href: "/images/favicon.ico" }]],
-      lang: "en-US",
-      title: "SAY DAO",
-      description: "Documentations for App and Panel",
-    },
+    // "/en/": {
+    //   head: [["link", { rel: "icon", href: "/images/favicon.ico" }]],
+    //   lang: "en-US",
+    // title: "SAY DAO",
+    //   description: "Documentations for App and Panel",
+    // },export default {
   },
   theme: defaultTheme({
-    logo: '/images/logo.png',
-    repo: 'SAY-DAO/road-map-docs',
-    docsDir: 'docs',
+    colorMode: "dark",
+    logo: "/images/logo.png",
+    repo: "SAY-DAO/road-map-docs",
+    docsDir: "docs",
     themePlugins: {
       // only enable git plugin in production mode
       git: isProd,
-      // use shiki plugin in production mode instead
       prismjs: !isProd,
     },
     locales: {
       "/": {
         selectLanguageName: "Farsi",
-        logo: "/images/logo2.png",
-        navbar: [
-          {
-            text: "API",
-            link: "/api/",
-          },
-        ],
+        logo: "/images/logo.png",
+        logoDark: "/images/logo2.png",
+        // navbar: [
+        //   {
+        //     text: "API",
+        //     link: "/api/",
+        //   },
+        // ],
         sidebar: {
           "/guide/": [
             {
               text: "مددکاران و سمن‌ها",
-              children: ["/guide/wallet.md", "/guide/keywords.md"],
+              children: [
+                "/guide/basics.md",
+                "/guide/wallet.md",
+                "/guide/keywords.md",
+              ],
             },
           ],
         },
       },
-      "/en/": {
-        selectLanguageName: "English",
-        logo: "/images/logo2.png",
-        navbar: [
-          { text: "Getting Started", link: "/getting-started/" },
-          {
-            text: "API",
-            link: "/api/",
-          },
-        ],
-        sidebar: [
-          {
-            text: "Wallets",
-            link: "/guide/",
-            children: [
-              {
-                text: "github",
-                link: "https://github.com",
-                children: [],
-              },
-              // string - page file path
-              "/guide/README.md",
-            ],
-          },
-          // string - page file path
-          "/guide/README.md",
-        ],
-      },
+      // "/en/": {
+      //   selectLanguageName: "English",
+      //   logo: "/images/logo2.png",
+      //   navbar: [
+      //     { text: "Getting Started", link: "/getting-started/" },
+      //     {
+      //       text: "API",
+      //       link: "/api/",
+      //     },
+      //   ],
+      //   sidebar: [
+      //     {
+      //       text: "Wallets",
+      //       link: "/guide/",
+      //       children: [
+      //         {
+      //           text: "github",
+      //           link: "https://github.com",
+      //           children: [],
+      //         },
+      //         // string - page file path
+      //         "/guide/README.md",
+      //       ],
+      //     },
+      //     // string - page file path
+      //     "/guide/README.md",
+      //   ],
+      // },
     },
   }),
   editLinks: false,
-  docsRepo: 'SAY-DAO/road-map-docs',
+  docsRepo: "SAY-DAO/road-map-docs",
   // use plugins
   plugins: [
-    docsearchPlugin({
-      appId: '34YFD9IUQ2',
-      apiKey: '9a9058b8655746634e01071411c366b8',
-      indexName: 'vuepress',
-      searchParameters: {
-        facetFilters: ['tags:v2'],
-      },
-
-    }),
+    // docsearchPlugin({
+    //   appId: "34YFD9IUQ2",
+    //   apiKey: "9a9058b8655746634e01071411c366b8",
+    //   indexName: "vuepress",
+    //   searchParameters: {
+    //     facetFilters: ["tags:v2"],
+    //   },
+    // }),
 
     registerComponentsPlugin({
-      componentsDir: path.resolve(__dirname, './theme/components'),
+      componentsDir: path.resolve(__dirname, "./theme/components"),
     }),
   ],
-
-});
+};
